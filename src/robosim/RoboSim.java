91c7adf17b011cf3c27cf27d9b1b4d8fd3fb3cef
Andraz Simcic <andraz.simcic1@gmail.com>
2017-12-28 16:11:08 +0100

    RE-INIT

--- src/robosim/RoboSim.java
+++ src/robosim/RoboSim.java
@@ -1,49 +1,8 @@
 package robosim;
 
-import robosim.graphics.*;
-import static org.lwjgl.glfw.GLFW.*;
-import org.lwjgl.opengl.GL;
-import org.lwjgl.opengl.GL11;
-import static org.lwjgl.opengl.GL11.*;
-
-
-/**
- *
- * @author Andraz & David
- */
 
 public class RoboSim {
     public static void main(String[] args) {
-        if(!glfwInit()) {
-            System.err.println("GLFW Failed to initialize!");
-            System.exit(1);
-        }
-        Window window = new Window(1280,720,"RoboSim");
         
-        window.makeContext();
-        GL.createCapabilities();
-        
-        while(true) {
-            glfwPollEvents();
-            if(window.shouldClose()) {
-                break;
-            }
-            glClear(GL_COLOR_BUFFER_BIT);
-            
-            //glBegin(GL_QUADS);
-            glBegin(GL_TRIANGLES);
-            glColor4f(1, 0, 0, 0);
-            glVertex3d(-0.5, 0.5, 0.5);
-            glColor4f(0, 1, 0, 0);
-            glVertex3d(0.5, -0.5, 0.5);
-            glColor4f(0, 0, 1, 0);
-            glVertex3d(-0.5, -0.5, 0.5);
-            glEnd();
-            
-            window.swapBuffer();
-        }
-        window.dispose();
-        
-        glfwTerminate();
     }
 }
---
+++ src/robosim/Window.java
@@ -0,0 +1,14 @@
+package robosim;
+
+
+public class Window {
+    
+    private long window;
+    private final int width = 1280;
+    private final int height = 720;
+    private final String title = "RoboSim";
+    
+    public Window() {
+        
+    }
+}
